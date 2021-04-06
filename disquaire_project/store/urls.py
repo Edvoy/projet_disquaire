675e1ea21f78d3fb1fd9622a11ff416e5194f604
Edvoy <edouard.voyer@gmail.com>
2021-04-06 17:07:07 +0200

    create models.py

--- disquaire_project/store/urls.py
+++ disquaire_project/store/urls.py
@@ -6,9 +6,11 @@ https://docs.djangoproject.com/fr/3.1/topics/http/urls/
 https://docs.djangoproject.com/fr/3.1/ref/urls/
 '''
 
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.listing, name="Listing"),
+    re_path(r'^(?P<album_id>[0-9]+)/$', views.detail),
+    re_path(r'^search/$', views.search),
     ]
--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -8,7 +8,7 @@ La vue elle-même contient la logique nécessaire pour renvoyer une réponse.
 
 from django.http import HttpResponse
 
-from .models import ALBUMS
+#from .models import ALBUMS
 
 def index(request):
     message = "Salut tout le monde !"
@@ -17,4 +17,34 @@ def index(request):
 def listing(request):
     albums = ["<li>{}</li>".format(album['name']) for album in ALBUMS]
     message = """<ul>{}</ul>""".format("\n".join(albums))
+    return HttpResponse(message)
+
+def detail(request, album_id):
+    id = int(album_id)
+    album = ALBUMS[id]
+    artists = " ".join([artist['name'] for artist in album['artists']])
+    message = "Le nom de l'album est {}. Il a été écrit par {}".format(album['name'], artists)
+    return HttpResponse(message)
+
+def search(request):
+    query = request.GET.get('query')
+    if not query:
+        message = "Aucun artiste n'est demandé"
+    else:
+        albums = [
+            album for album in ALBUMS
+            if query in " ".join(artist['name'] for artist in album['artists'])
+        ]
+
+        if len(albums) == 0:
+            message = "Misère de misère, nous n'avons trouvé aucun résultat !"
+        else:
+            albums = ["<li>{}</li>".format(album['name']) for album in albums]
+            message = """
+                Nous avons trouvé les albums correspondant à votre requête ! Les voici :
+                <ul>
+                    {}
+                </ul>
+            """.format("</li><li>".join(albums))
+
     return HttpResponse(message)
