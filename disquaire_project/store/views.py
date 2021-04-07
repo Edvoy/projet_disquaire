f90d41cb9879c5c33cf5f3b0bee0884c456186c1
Edvoy <edouard.voyer@gmail.com>
2021-04-07 19:03:59 +0200

    add paginator

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -7,6 +7,7 @@ La vue elle-même contient la logique nécessaire pour renvoyer une réponse.
 '''
 
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from .models import Album, Artist, Contact, Booking
 
@@ -20,11 +21,22 @@ def index(request):
     return render(request, 'store/index.html', context)
 
 def listing(request):
-    albums = Album.objects.filter(available=True).order_by('-created_at')
+    albums_list = Album.objects.filter(available=True)
+    paginator = Paginator(albums_list, 9)
+    page = request.GET.get('page')
+    try:
+        albums = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        albums = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        albums = paginator.page(paginator.num_pages)
     context = {
-        'album': albums
+        'albums': albums,
+        'paginate': True
     }
-    return render(request, 'store/listening.html', context)
+    return render(request, 'store/listing.html', context)
 
 def detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
